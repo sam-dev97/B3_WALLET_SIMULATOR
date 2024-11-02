fcd6b26845f7615da77ed0090f52e7fdee1c36da
Samuel Santos <vivatecnosams@gmail.com>
2024-11-02 21:25:24 +0000

    Test css files

--- pj_wallet/settings.py
+++ pj_wallet/settings.py
@@ -104,7 +104,7 @@ import os
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
